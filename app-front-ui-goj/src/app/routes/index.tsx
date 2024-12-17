244caf82b04fa20923ee3fe0d3ee84f5fb7eb534
qumanqul <defople@gmail.com>
2024-12-17 20:58:48 +0100

    Added Map.js with interactive map

--- app-front-ui-goj/src/app/routes/index.tsx
+++ app-front-ui-goj/src/app/routes/index.tsx
@@ -1,23 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../../Layout";
 import React from "react";
-import Home from "../pages/HomePage";
-import NotFoundException from "../exceptions/NotFoundException";
+import Home from "../pages/HomePage.tsx";
+import NotFoundException from "../exceptions/NotFoundException.tsx";
+import Map from "../pages/Map";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout />,
-        children: [
-            { path: "", element: <Home />},
-        ]
-    },
-    {
-        path: "501",
-        element: null
-    },
-    {
-        path: "*",
-        element: <NotFoundException />
-    },
-])
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "map", element: <Map /> },
+    ],
+  },
+  {
+    path: "501",
+    element: null,
+  },
+  {
+    path: "*",
+    element: <NotFoundException />,
+  },
+]);
