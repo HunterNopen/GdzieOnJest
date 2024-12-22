fc87780f9210078129e8300c53430260a6e887c4
qumanqul <defople@gmail.com>
2024-12-22 13:58:52 +0100

    GOJ-26 - Add function to searching bus using schedule

--- app-back-goj/index.js
+++ app-back-goj/index.js
@@ -3,7 +3,7 @@ const busRoutes = require("./src/routes/busRoutes");
 const express = require("express");
 const app = express();
 
-app.use("/search", busRoutes); // http://localhost:3000/search?from=Gdańsk, Dworzec Autobusowy&to=Test&time=14:00&day=weekdays
+app.use("/search", busRoutes); // Example url: http://localhost:3000/search?from=Gdańsk, Dworzec Autobusowy&to=Test&time=14:00&day=weekdays
 
 app.get("/", (req, res) => {
   res.send("GdzieOnJest Welcomes Urbanowicz!");
